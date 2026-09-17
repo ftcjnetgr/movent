5bdee12ce53a0eed9904e6bb9d827f7c1759f909
ftcjnetgr <transportjnetgr@gmail.com>
2026-09-17 23:54:21 +0700

    Fix report end-date inclusion

--- lib/server/report.ts
+++ lib/server/report.ts
@@ -23,6 +23,12 @@ function csvText(rows: Record<string, unknown>[]) {
   ].join('\r\n')
 }
 
+function endExclusiveIso(date: string) {
+  const end = new Date(`${date}T00:00:00+07:00`)
+  end.setUTCDate(end.getUTCDate() + 1)
+  return end.toISOString()
+}
+
 export async function queryOperationalReport(filters: ReportFilters) {
   const admin = createAdminClient()
   const dateField = filters.type === 'STD' ? 'std' : filters.type === 'STA' ? 'sta' : 'canceled_at'
@@ -31,7 +37,7 @@ export async function queryOperationalReport(filters: ReportFilters) {
     .from('tasks')
     .select('transaction_id, task_type, status, start_point, destination, std, sta, executor_nik, executor_snapshot, fleet_snapshot, canceled_at, cancellation_note')
     .gte(dateField, `${filters.from}T00:00:00+07:00`)
-    .lt(dateField, `${filters.to}T00:00:00+07:00`)
+    .lt(dateField, endExclusiveIso(filters.to))
     .order(dateField, { ascending: true })
 
   if (filters.startPoint) query = query.eq('start_point', filters.startPoint)
