876d63ac935c1f9243027a521b0ef916d12ca0a5
ftcjnetgr <transportjnetgr@gmail.com>
2026-09-18 10:28:56 +0700

    feat: add duration visualizations to maintainer

--- app/maintainer/ticketing-maintenance/page.tsx
+++ app/maintainer/ticketing-maintenance/page.tsx
@@ -1,4 +1,5 @@
 import AppShell from '@/components/app-shell'
+import { TaskDurationVisualization, TicketDurationVisualization } from '@/components/duration-visualization'
 import DashboardAlertList from '@/components/dashboard-alert-list'
 import { getDashboardData } from '@/lib/server/dashboard'
 import { getCurrentProfile } from '@/lib/server/profile'
@@ -22,6 +23,8 @@ export default async function MaintainerTicketingMaintenanceDashboardPage() {
         </div>
       </section>
       <DashboardAlertList ticketAlerts={data.ticketAlerts} />
+
+      <TicketDurationVisualization rows={data.ticketDurations} />
     </AppShell>
   )
 }
