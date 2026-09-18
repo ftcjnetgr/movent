66262bf965596fb581ebcd16cceceed9b3321fff
ftcjnetgr <transportjnetgr@gmail.com>
2026-09-18 18:07:55 +0700

    perf(ui): persist shell during dispatcher navigation

--- app/dispatcher/extra-schedule/page.tsx
+++ app/dispatcher/extra-schedule/page.tsx
@@ -1,10 +1,6 @@
-import AppShell from '@/components/app-shell'
 import DispatcherExtraSchedulePage from '@/components/dispatcher-extra-schedule-page'
 
 export default function DispatcherExtraScheduleRoute() {
   return (
-    <AppShell>
-      <DispatcherExtraSchedulePage />
-    </AppShell>
-  )
+    <DispatcherExtraSchedulePage />)
 }
